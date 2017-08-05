42b6d2b47e5f020a6fd7be959e0502431497ac60
Nicholas Scheel <monoidmusician@gmail.com>
2017-08-04 21:20:35 -0500

    Proper PROPER errors in Routerify

--- src/Routing/Routes.purs
+++ src/Routing/Routes.purs
@@ -1,33 +1,42 @@
 module Routing.Routes where
 
-import Prelude
-
-import Control.Apply (applyFirst, applySecond, lift2)
+import Control.Apply (applyFirst, applySecond)
 import Control.Plus (empty, (<|>))
+import Data.Array (fromFoldable)
 import Data.Bifunctor (bimap)
-import Data.Either (Either(..), either, hush, note)
+import Data.Either (Either(..), either)
 import Data.Foldable (foldMap)
 import Data.Functor.Contravariant (class Contravariant, cmap)
-import Data.Lens (_Just, _Nothing, preview, review)
+import Data.Lens (preview, review)
 import Data.Lens.Prism (prism', only)
+import Data.Lens.Prism.Either (_Left, _Right)
 import Data.Lens.Types (Prism')
 import Data.List (List(..), (:))
 import Data.Map (isEmpty, singleton, toUnfoldable)
-import Data.Maybe (Maybe(..))
+import Data.Maybe (Maybe(..), maybe)
+import Data.Monoid (mempty)
 import Data.Newtype (class Newtype, unwrap)
-import Data.String (drop)
+import Data.Semiring.Free (Free(..), free)
+import Data.String (drop, joinWith)
 import Data.Tuple (Tuple(..), uncurry)
+import Data.Validation.Semiring (V, invalid, unV)
 import Partial.Unsafe (unsafePartialBecause)
+import Prelude hiding (discard)
 import Routing (match)
-import Routing.Match (Match)
-import Routing.Match.Class (class MatchClass, end, int, lit)
+import Routing.Match (Match(..))
+import Routing.Match.Class (class MatchClass, end, int, lit, str)
 import Routing.Types (Route, RoutePart(..))
 
 -- | Mostly duplicated operations from elsewhere bundled up to work similar to
 -- | http://www.informatik.uni-marburg.de/~rendel/unparse/rendel10invertible.pdf
 class Combinators c where
   -- | C.f. Control.Alt.empty.
-  emptyVal :: forall a. c a
+  emptyFail :: forall a. c a
+  -- | Uh don't ask me...
+  emptySuccess :: c Unit
+  -- | The url may include this but it will not be generated.
+  -- | This has different semantics for matching versus displaying ...
+  allowed :: c Unit -> c Unit
   -- | (<||>) C.f. Control.Alt.alt.
   eitherOr :: forall a. c a -> c a -> c a
   -- | (<:>) A sort of map that works either covariantly or contravariantly with
@@ -51,7 +60,9 @@ infixl 7 before as </
 
 -- | Matching is an instance of Combinators, works like you would expect.
 instance combmatch :: Combinators Match where
-  emptyVal = empty
+  emptyFail = empty
+  emptySuccess = Match (pure <<< (Tuple <@> unit))
+  allowed f = optional f
   eitherOr = (<|>)
   prismMap p = map (review p)
   withCurry a b = Tuple <$> a <*> b
@@ -59,23 +70,21 @@ instance combmatch :: Combinators Match where
   before = applyFirst
 
 -- | A contravariant functor for building up a route to be converted to a string.
-newtype Routerify a = Routerify (a -> Either String Route)
+newtype Routerify a = Routerify (a -> V (Free String) Route)
 derive instance newtypeRouterify :: Newtype (Routerify a) _
 
 instance contrafunctorRouterify :: Contravariant Routerify where
   cmap f (Routerify p) = Routerify (p <<< f)
 
 instance combRouterify :: Combinators Routerify where
-  emptyVal = Routerify (const (Left "no match"))
-  eitherOr (Routerify l) (Routerify r) = Routerify \a ->
-    case l a, r a of
-      Right la, _ -> Right la
-      _, Right ra -> Right ra
-      Left la, Left ra -> Left (la <> ra)
+  emptyFail = Routerify (const (invalid (free "no match")))
+  emptySuccess = Routerify (const (pure mempty))
+  allowed _ = emptySuccess
+  eitherOr (Routerify l) (Routerify r) = Routerify \a -> l a <|> r a
   prismMap p (Routerify r) = Routerify
-    (preview p >>> map r >>> note "prism failed to match" >>> join)
+    (preview p >>> maybe (invalid (free "prism failed to match")) r)
   withCurry (Routerify l) (Routerify r) = Routerify
-    (bimap l r >>> uncurry (lift2 append))
+    (bimap l r >>> uncurry append)
   andThen l r = cmap (Tuple unit) (withCurry l r)
   before l r = cmap (Tuple <@> unit) (withCurry l r)
 
@@ -90,14 +99,14 @@ instance matchclassRouterify :: MatchClass Routerify where
   num = Routerify (ppPath <<< show)
   -- | Show an `Int` as a URL part.
   int = Routerify (ppPath <<< show)
-  -- | Show a `Boolean` as a URL part.
+  -- | Show a `Boolean` as a URL part ("true" and "false").
   bool = Routerify (ppPath <<< if _ then "true" else "false")
   -- | Show a `String` as a URL part.
   str = Routerify (ppPath)
-  -- | No-op, has semantic value when matching.
+  -- | No-op, would have semantic value when matching.
   end = Routerify (const (pure empty))
   -- | Provide an error message.
-  fail = Routerify <<< const <<< Left
+  fail = Routerify <<< const <<< invalid <<< free
   -- | Show a single parameter.
   param p = Routerify (pure <<< pure <<< Query <<< singleton p)
   -- | Show a bunch of parameters.
@@ -114,19 +123,19 @@ instance showLocations :: Show Locations where
   show Home = "Home"
   show Dashboard = "Dashboard"
   show (Project i) = "Project " <> show i
--- | A `Nothing` value indicates a 404 not found error. See `Prism'`s below.
-type Location = Maybe Locations
+-- | A `Left` value indicates a 404 not found error. See `Prism'`s below.
+type Location = Either String Locations
 
 _Home :: Prism' Location Unit
-_Home = _Just <<< only Home
+_Home = _Right <<< only Home
 _Dashboard :: Prism' Location Unit
-_Dashboard = _Just <<< only Dashboard
+_Dashboard = _Right <<< only Dashboard
 _Project :: Prism' Location Int
-_Project = _Just <<< prism' Project case _ of
+_Project = _Right <<< prism' Project case _ of
   Project i -> Just i
   _ -> Nothing
-_NotFound :: Prism' Location Unit
-_NotFound = _Nothing
+_NotFound :: Prism' Location String
+_NotFound = _Left
 
 -- | A slash value.
 slash :: forall m. MatchClass m => m Unit
@@ -140,13 +149,17 @@ dir = slash /> end
 -- | This may end with a slash.
 dirish :: forall m. Combinators m => MatchClass m => m Unit
 dirish = end <||> dir
--- | The url may include this but it will not be generated.
-allowed :: forall m. Combinators m => MatchClass m => m Unit -> m Unit
-allowed = eitherOr emptyVal
 -- | The url should include this (i.e. it will be printed), but it does not have
 -- | to.
 optional :: forall m. Combinators m => MatchClass m => m Unit -> m Unit
-optional = eitherOr <@> emptyVal
+optional = eitherOr <@> emptySuccess
+-- | Match a literal and return it as a `String`.
+matchlit :: forall m. Combinators m => MatchClass m => String -> m String
+matchlit v = only v <:> lit v
+
+-- | Join a list of combinators with `eitherOr`.
+discard :: forall m a. Combinators m => m a -> (Unit -> m a) -> m a
+discard a f = a <||> f unit
 
 -- | The `MatchClass` value for a `Location`.
 -- |   - `_Home` is "/" or "/home", etc.
@@ -154,15 +167,24 @@ optional = eitherOr <@> emptyVal
 -- |   - `_Project` is "project/{int}", idem
 -- |   - `_NotFound` maps to `404` (idem), but can represent any other location.
 loc :: forall m. Combinators m => MatchClass m => m Location
-loc =  _Home      <:> slashish (allowed  (lit "home" /> dirish))
-  <||> _Dashboard <:> slashish (lit "dashboard"      /> dirish)
-  <||> _Project   <:> slashish (lit "project" /> int </ dirish)
-  <||> _NotFound  <:> slashish (lit "404"            /> dirish)
+loc = do
+  _Home      <:> slashish (allowed (lit "home") </ dirish)
+  _Dashboard <:> slashish (lit "dashboard"      </ dirish)
+  _Project   <:> slashish (lit "project" /> int </ dirish)
+  _NotFound  <:> slashish match404
+  where
+    match404 = do
+      -- "404" --> 404
+      matchlit "404"
+      -- "" --> 404
+      only "" <:> lit "404"
+      -- reason --> 404/{reason}
+      lit "404" /> str
 
 -- | Parses a `String` into a `Location`, giving any failures to `_NotFound`
--- | aka `Nothing`.
+-- | aka `Left`.
 parseloc :: String -> Location
-parseloc = match loc >>> hush >>> join
+parseloc l = match loc l # either (const (Left ("url_parse_error/" <> l))) id
 
 -- | Show a `Route` as a `String`.
 showroute :: Route -> String
@@ -188,6 +210,11 @@ showroute r = go r
       \(Tuple p v) -> "&" <> p <> "=" <> v
 
 -- | Show a `Location` as a `String`. Should be the partial inverse of
--- | `parseloc`. `_NotFound` gets rendered as "/404/".
+-- | `parseloc`. `_NotFound` gets rendered as "/404/...".
+-- | Errors are, somewhat unforunately, printed into the result string ...
 showloc :: Location -> String
-showloc = unwrap (loc :: Routerify Location) >>> either id showroute
+showloc = unwrap (loc :: Routerify Location) >>> flip unV showroute \(Free e) ->
+  showerrorzies e
+
+showerrorzies :: List (List String) -> String
+showerrorzies e = joinWith "; " $ fromFoldable $ joinWith ", " <<< fromFoldable <$> e
