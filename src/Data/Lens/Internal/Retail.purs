8dddbc53969dd5bce3013b79e597c22c3c0fba5e
Nicholas Scheel <monoidmusician@gmail.com>
2017-08-06 14:54:30 -0500

    Fix Retail module
    Remove Choice instance that used Either String.

--- src/Data/Lens/Internal/Retail.purs
+++ src/Data/Lens/Internal/Retail.purs
@@ -14,12 +14,6 @@ data Retail f s t a b = Retail (a -> f s) (t -> f b)
 instance profunctorRetail :: Functor f => Profunctor (Retail f s t) where
   dimap f g (Retail l r) = Retail (l <<< f) (map g <<< r)
 
-instance choiceRetailEither :: Choice (Retail (Either String) s t) where
-  left (Retail as st) = Retail
-    (either as (\_ -> Left "semi-iso failed")) (map Left <<< st)
-  right (Retail as st) = Retail
-    (either (\_ -> Left "semi-iso failed") as) (map Right <<< st)
-
 instance choiceRetailMaybe :: Choice (Retail Maybe s t) where
   left (Retail as st) = Retail
     (either as (const Nothing)) (map Left <<< st)
