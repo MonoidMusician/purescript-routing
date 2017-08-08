8802be2229bf08ed746a6f2c34007cbe036c3705
Nicholas Scheel <monoidmusician@gmail.com>
2017-08-07 19:28:02 -0500

    hoist semi isos, cleanup

--- src/Data/Lens/SemiIso.purs
+++ src/Data/Lens/SemiIso.purs
@@ -19,7 +19,7 @@ type ASemiIso f s t a b = Optic (Retail f a b) s t a b
 type ASemiIso' f s a = ASemiIso f s s a a
 
 semiIso :: forall f s t a b. (s -> f a) -> (b -> f t) -> SemiIso f s t a b
-semiIso sa bt = merge <<< dimap sa bt <<< expose
+semiIso sfa bft = merge <<< dimap sfa bft <<< expose
 
 apply :: forall f s t a b. Applicative f => ASemiIso f s t a b -> s -> f a
 apply l = withSemiIso l const
@@ -28,32 +28,55 @@ apply l = withSemiIso l const
 unapply :: forall f s t a b. Applicative f => ASemiIso f s t a b -> b -> f t
 unapply l = withSemiIso l (const id)
 
---compose :: forall f x b s y a t. Bind f => ASemiIso f x y a b -> ASemiIso f s t x y -> SemiIso f s t a b
+-- | Compose two `SemiIso'`s. In order to get this to respect typing it might be
+-- | necessary to inline the definition of this, be warned!
 compose :: forall f s a x. Bind f => ASemiIso' f x a -> ASemiIso' f s x -> SemiIso' f s a
-compose sxay xtyb =
-  let
-    Retail f f' = sxay (Retail pure pure)
-    Retail g g' = xtyb (Retail pure pure)
-  in semiIso (f <=< g) (g' <=< f')
+compose xaiax sxixs =
+  withSemiIso xaiax \xfa afx ->
+    withSemiIso sxixs \sfx xfs ->
+      semiIso
+        -- once we have the transformations
+        -- compose them Kleisli style
+        (sfx >=> xfa) -- s -> f a
+        (afx >=> xfs) -- a -> f s
 
+-- | Unwraps the two functions contained in a `Retail`.
 withSemiIso :: forall f s t a b r. Applicative f => ASemiIso f s t a b -> ((s -> f a) -> (b -> f t) -> r) -> r
-withSemiIso ai k = case ai (Retail pure pure) of
-  Retail sa bt -> k sa bt
+withSemiIso stiab k =
+  -- apply the steps of semiIso to Retail pure pure to get Retail f g
+  --            Retail pure pure
+  -- expose:    Retail (bind pure) pure
+  -- a.k.a.:    Retail id pure
+  -- dimap f g: Retail f (map g <<< pure)
+  -- a.k.a.:    Retail f (pure <<< g)
+  -- merge:     Retail f (join <<< pure <<< g)
+  -- a.k.a.:    Retail f g
+  case stiab (Retail pure pure) of
+    Retail sfa bft -> k sfa bft
 
+-- | Hoist the error monad that the semi-isomorphism operates in.
+hoist :: forall f g s t a b. Applicative f => (f ~> g) -> ASemiIso f s t a b -> SemiIso g s t a b
+hoist nt l = withSemiIso l \sfa bft -> semiIso (nt <<< sfa) (nt <<< bft)
+
+note :: forall s t a b e. e -> ASemiIso Maybe s t a b -> SemiIso (Either e) s t a b
+note e l = hoist (E.note e) l
+
+hush :: forall s t a b e. ASemiIso (Either e) s t a b -> SemiIso Maybe s t a b
+hush l = hoist E.hush l
+
+-- | Every `Prism` is a `SemiIso`.
 fromPrism :: forall s t a b. APrism s t a b -> SemiIso Maybe s t a b
 fromPrism l = withPrism l \bt sa -> semiIso (E.hush <<< sa) (pure <<< bt)
 
-note :: forall s t a b e. e -> ASemiIso Maybe s t a b -> SemiIso (Either e) s t a b
-note e l = withSemiIso l \sa bt -> semiIso (E.note e <<< sa) (E.note e <<< bt)
-
-hush :: forall s t a b e. ASemiIso (Either e) s t a b -> SemiIso Maybe s t a b
-hush l = withSemiIso l \sa bt -> semiIso (E.hush <<< sa) (E.hush <<< bt)
-
+-- | Every `Iso` is a `SemiIso`.
 weaken :: forall f s t a b. Applicative f => AnIso s t a b -> SemiIso f s t a b
 weaken l = withIso l \sa bt -> semiIso (pure <<< sa) (pure <<< bt)
 
+-- | If the error monad is just `Identity`, then a `SemiIso` is really an `Iso`.
 strengthen :: forall s t a b. ASemiIso Identity s t a b -> Iso s t a b
-strengthen l = withSemiIso l \sa bt -> iso (unwrap <<< sa) (unwrap <<< bt)
+strengthen l = withSemiIso l \sfa bft -> iso (unwrap <<< sfa) (unwrap <<< bft)
 
+-- | Always generates a `unit` value or the `a` passed in, depending on
+-- | direction.
 constant :: forall f a. Applicative f => a -> SemiIso' f Unit a
-constant a = semiIso (pure (pure a)) (pure (pure unit))
+constant a = semiIso (const (pure a)) (const (pure unit))
